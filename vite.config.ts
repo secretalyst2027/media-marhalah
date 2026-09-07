36725da9654f2475a9883a3d0762d4182fe9220f
secretalyst2027 <secretalyst2027@gmail.com>
2026-09-07 23:51:22 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,20 @@
 import { defineConfig } from "vite";
 import { vitePlugin as remix } from "@remix-run/dev";
-import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
-import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
 export default defineConfig({
   plugins: [
-    netlifyPlugin(),
     tailwindcss(),
     remix({
-      
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
-        v3_lazyRouteDiscovery: true,
-        v3_singleFetch: true,
       },
     }),
+    netlifyPlugin(),
   ],
   resolve: {
     alias: {
